cda3c127832903d6adb455346979b0a8baab1699
tramteja <ramtejatadishetti@gmail.com>
2017-03-27 16:05:48 -0400

    Removed all un-necesary files, added wrapper for making hyphenated words

---
+++ wrapper_for_storing_phrases.py
@@ -0,0 +1,38 @@
+import os,re
+import pickle
+import csv
+
+#List of files that needs to be processed
+fileList = {};
+datapath = "./Phrases_semantria"
+datafile = os.listdir(datapath);
+for files in datafile:
+    if re.match(".*\.csv",files):
+        filepath = os.path.join(datapath, files);
+        fileList[files] = filepath;
+
+phrase_list = []
+# make a pickle of phrases by reading
+for entry in fileList.keys():
+    with open(fileList[entry],'rb') as f:
+        reader = csv.reader(f) 
+        for row in reader:
+            words_in_phrases = row[0].split()
+            single_word = words_in_phrases[0]
+            if len(words_in_phrases) > 1:
+                for i in range(1, len(words_in_phrases)):
+                    single_word += '-'
+                    single_word += words_in_phrases[i]
+
+            phrase_list.append(single_word)
+
+    result_name = "result" + entry + ".pickle"
+    with open(result_name, 'wb') as handle:
+        pickle.dump(phrase_list, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+
+
+
+
+
